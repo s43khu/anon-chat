7e14d88ed9e04c27a65323094c913b4c02cf57a1
shekhu <s43khu@gmail.com>
2024-06-24 07:36:20 +0530

    login adjust

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -7,51 +7,40 @@ import styled from "styled-components";
 
 export const Login = () => {
   return (
-    <>
-      <LoginContainer>
-        <LogoImage src={logo} alt="logo" />
-        <LoginHeader>Welcome to my chat app!!</LoginHeader>
-        <LoginDescription>
-          Made with 💚 by{" "}
-          <a href="https://github.com/maciekt07/" target="_blank">
-            maciekt07
-          </a>{" "}
-          using{" "}
-          <a href="https://supabase.com/" target="_blank">
-            Supabase
-          </a>
-          . <br /> <b>Sign in to get started.</b>
-        </LoginDescription>
-        <AuthContainer>
-          <Auth
-            supabaseClient={supabase as SupabaseClient}
-            providers={["github", "google"]}
-            appearance={{
-              theme: ThemeSupa,
-              variables: {
-                default: {
-                  colors: {
-                    brand: "#6b0fff",
-                    brandAccent: "#7c2bff",
-                  },
-                  space: {
-                    buttonPadding: "12px",
-                    inputPadding: "12px",
-                  },
-                  radii: {
-                    borderRadiusButton: "12px",
-                    buttonBorderRadius: "12px",
-                    inputBorderRadius: "12px",
-                  },
+    <LoginContainer>
+      <LogoImage src={logo} alt="logo" />
+      <LoginHeader>Welcome to my chat app!!</LoginHeader>
+      <LoginDescription>
+        <br /> <b>Sign in to get started.</b>
+      </LoginDescription>
+      <AuthContainer>
+        <Auth
+          supabaseClient={supabase as SupabaseClient}
+          appearance={{
+            theme: ThemeSupa,
+            variables: {
+              default: {
+                colors: {
+                  brand: "#6b0fff",
+                  brandAccent: "#7c2bff",
+                },
+                space: {
+                  buttonPadding: "12px",
+                  inputPadding: "12px",
+                },
+                radii: {
+                  borderRadiusButton: "12px",
+                  buttonBorderRadius: "12px",
+                  inputBorderRadius: "12px",
                 },
               },
-            }}
-            theme="dark"
-            magicLink
-          />
-        </AuthContainer>
-      </LoginContainer>
-    </>
+            },
+          }}
+          theme="dark"
+          magicLink
+        />
+      </AuthContainer>
+    </LoginContainer>
   );
 };
 
